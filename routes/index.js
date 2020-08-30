ae3274235fefd4ffad3d055e3ea09841ece9c50b
tom_glb <goodMorning_glb@atguigu.com>
2020-08-30 21:21:52 +0800

    登录注册

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,22 @@ var model = require('../model')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   model.connect(function(db){
-    db.collection('users').find().toArray(function(err,docs){ // 查询
+    db.collection('users').find().toArray(function(err,docs){ // 查询,转换数组
       console.log('用过户列表',docs)
-      res.render('index', { title: 'Express' }); // 响应客户端
+      var data = JSON.stringify(docs)
+      res.render('index', { title: 'Express',data}); // 响应客户端
     })
   })
 });
+
+// 渲染注册页面
+router.get('/regist',function(req, res, next){
+  res.render('regist', {})
+})
+
+// 渲染登录页面
+router.get('/login',function(req, res, next){
+  res.render('login', {}) // 页面传递数据
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var model =  require('../model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// 注册接口
+router.post('/regist', function(req, res, next) {
+ var data = {
+   username: req.body.username,
+   password: req.body.password,
+   password2: req.body.password2
+ }
+ // 校验数据
+ model.connect(function(db){
+   db.collection('user').insertOne(data,function(err,ret){
+    if(err){
+      console.log('注册失败')
+      res.redirect('/regist')
+    } else {
+      res.redirect('/login')
+    }
+   })
+ })
+//  res.send(data) // 路由页面打印显示数据
+});
+
+// 登录接口
+router.post('/login', function(req, res, next) {
+  var data = {
+    username: req.body.username,
+    password: req.body.password,
+  }
+  // 校验数据
+  // model.connect(function(db){    
+  //   db.collection('user').insertOne(data,function(err,ret){
+  //    if(err){
+  //      console.log('注册失败')
+  //      res.redirect('/regist')
+  //    } else {
+  //      res.redirect('/login')
+  //    }
+  //   })
+  // })
+  console.log('data:',data) //  控制台打印
+ });
 
 module.exports = router;
